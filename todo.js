8e53b89ba02cd62fc3de0b8c98a462297c2694d7
Aakash Kumar Suman <121715040+aakashks13@users.noreply.github.com>
2024-05-02 15:54:38 +0530

    todo.js

--- todo.js
+++ todo.js
@@ -1,36 +1,36 @@
-// //simple to-do app
-// let todoList = [];
-// let request = prompt("Your Request please").toLowerCase().trim();
-// for(let i = 0; ; i++){
-//     if(request === "done"){
-//         console.log("Thanks");
-//         break;
-//     }
-//     if(request === "list"){
-//         if(todoList.length === 0){
-//             console.log("No Task to show")
-//         }else{
-//             for(let i = 0; i < todoList.length; i++){
-//                 console.log(i, todoList[i]);
-//             }
-//         }
-//     }else if(request === "add"){
-//         let addTask = prompt("Add your Task");
-//         todoList.push(addTask);
-//         console.log("Task Added");
-//     }else if(request === "delete"){
-//         let deleteTask = Number(prompt("Enter index Number"));
-//         if(deleteTask >= todoList.length){
-//             console.log("No such index exist in your todo List");
-//         }else{
-//             todoList.splice(deleteTask, 1);
-//             console.log(`Task with index ${deleteTask} deleted`);
-//         }
-//     }else if(request === "reset"){
-//         todoList.splice(0, todoList.length);
-//         console.log("All Tasks removed");
-//     }else{
-//         console.log("Wrong Request");
-//     }
-//     request = prompt("Your Request Please").toLowerCase().trim();
-// }
+// simple to-do app
+let todoList = [];
+let request = prompt("Your Request please").toLowerCase().trim();
+for(let i = 0; ; i++){
+   if(request === "done"){
+        console.log("Thanks");
+        break;
+    }
+    if(request === "list"){
+        if(todoList.length === 0){
+            console.log("No Task to show")
+        }else{
+            for(let i = 0; i < todoList.length; i++){
+                console.log(i, todoList[i]);
+            }
+        }
+    }else if(request === "add"){
+        let addTask = prompt("Add your Task");
+        todoList.push(addTask);
+        console.log("Task Added");
+    }else if(request === "delete"){
+        let deleteTask = Number(prompt("Enter index Number"));
+        if(deleteTask >= todoList.length){
+            console.log("No such index exist in your todo List");
+        }else{
+            todoList.splice(deleteTask, 1);
+            console.log(`Task with index ${deleteTask} deleted`);
+        }
+    }else if(request === "reset"){
+        todoList.splice(0, todoList.length);
+        console.log("All Tasks removed");
+    }else{
+        console.log("Wrong Request");
+    }
+    request = prompt("Your Request Please").toLowerCase().trim();
+}
